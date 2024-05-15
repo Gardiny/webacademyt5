3524d5d4be8c782484f925cfe0d4725caa835f58
Gardiny <igor20091983@gmail.com>
2024-05-15 19:59:21 -0300

    dia15

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let botao = document.querySelector('a#add');
+// let botao = document.querySelector('a#add');
 // botao.addEventListener('click', () => {alert('Cliclado')});
-botao.addEventListener('click', funcaoA);
-botao.addEventListener('click', funcaoA);
-function funcaoA() {
-    alert('A');
-}
-function funcaoB() {
-    alert('B');
-}
+// botao.addEventListener('click', funcaoA);
+// botao.addEventListener('click', funcaoA);
+// function funcaoA() {
+//     alert('A');
+// }
+// function funcaoB() {
+//     alert('B');
+// }
+
+let selectTema = document.querySelector('select#tema')
+selectTema.addEventListener('change', evento => {
+    let temaSelecionado = evento.target.value;
+    console.log(temaSelecionado);
+})
